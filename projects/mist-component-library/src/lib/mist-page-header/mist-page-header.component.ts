6775d16062c03e297e3fab757aeda8d4ad76645c
Edward Honour <ed@tritaniumlabs.com>
2023-02-15 14:57:07 -0600

    insane

---
+++ projects/mist-component-library/src/lib/mist-page-header/mist-page-header.component.ts
@@ -0,0 +1,10 @@
+npm import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-mist-page-header',
+  templateUrl: './mist-page-header.component.html',
+  styleUrls: ['./mist-page-header.component.css']
+})
+export class MistPageHeaderComponent {
+
+}
